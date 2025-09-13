e92b8adb10de1d4274467fadba0ca9bbf26e3ce6
harvestsure <harvestsure@gmail.com>
2025-09-13 14:59:00 +0800

    fix bugs

--- components/code-block.tsx
+++ components/code-block.tsx
@@ -68,29 +68,33 @@ export function CodeBlock({ code, language }: CodeBlockProps) {
     <div className="relative h-full flex flex-col bg-gray-50 dark:bg-gray-900 rounded-lg overflow-hidden">
       {/* Code content */}
       <div className="flex-1 overflow-auto">
-        <SyntaxHighlighter
-          language={prismLanguage}
-          style={theme === "dark" ? oneDark : oneLight}
-          customStyle={{
-            margin: 0,
-            padding: "1rem",
-            background: "transparent",
-            fontSize: "14px",
-            lineHeight: "1.6",
-            height: "100%",
-          }}
-          showLineNumbers={true}
-          lineNumberStyle={{
-            minWidth: "3em",
-            paddingRight: "1em",
-            color: theme === "dark" ? "#6b7280" : "#9ca3af",
-            userSelect: "none",
-          }}
-          wrapLines={true}
-          wrapLongLines={true}
-        >
-          {code}
-        </SyntaxHighlighter>
+        <div style={{ userSelect: "text" }}>
+          <SyntaxHighlighter
+            language={prismLanguage}
+            style={theme === "dark" ? oneDark : oneLight}
+            customStyle={{
+              margin: 0,
+              padding: "1rem",
+              background: "transparent",
+              fontSize: "14px",
+              lineHeight: "1.6",
+              height: "100%",
+            }}
+            showLineNumbers={true}
+            lineNumberStyle={{
+              minWidth: "3em",
+              paddingRight: "1em",
+              color: theme === "dark" ? "#6b7280" : "#9ca3af",
+              userSelect: "none",
+              
+            }}
+            // wrapLines={true}   // 删除
+            // wrapLongLines={true}   // 删除
+            PreTag="pre"
+          >
+            {code}
+          </SyntaxHighlighter>
+        </div>
       </div>
     </div>
   )
